c5693c1340fd5a49652332080d282f906791366e
Jorn Ola Birkeland <jornola.birkeland@gmail.com>
2009-09-28 12:47:45 +0200

    Create GeneralWorkProcess to replace ConwipWorkProcess and KanbanWorkProcess

---
+++ Flow/GeneralWorkProcess.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MonteCarloFlow;
+using MonteCarloFlowTest;
+
+namespace Flow
+{
+    public class GeneralWorkProcess : WorkProcess, IWorkProcess
+    {
+        private readonly List<IWorkStation> _workStations = new List<IWorkStation>();
+        private readonly DoneQueue _doneQueue = new DoneQueue();
+
+        public GeneralWorkProcess(IWorkStation backlog)
+        {
+            _workStations.Add(backlog);
+            _workStations.Add(_doneQueue);
+        }
+
+        public void Tick(long ticks)
+        {
+            while (ticks-- > 0)
+            {
+                foreach (IWorkStation station in _workStations)
+                {
+                    station.Tick();
+                }
+
+                for (int index = _workStations.Count - 1; index > 0; index--)
+                {
+                    while (_workStations[index - 1].HasFinishedJobs)
+                    {
+                        IWorkItemTransition workItemTransition = _workStations[index - 1].BeginWorkItemTransition();
+                        if(!_workStations[index].TryAddWorkItem(workItemTransition))
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public int CompletionQueueCount
+        {
+            get { return _doneQueue.Count; }
+        }
+
+        public IEnumerable<WorkItem> CompletedJobs
+        {
+            get { return _doneQueue; }
+        }
+
+
+        protected override void OnAdd(WorkStation station)
+        {
+            _workStations.Insert(_workStations.Count - 1, station);
+        }
+    }
+}
---
+++ MonteCarloFlowTest/GeneralWorkProcessTest.cs
@@ -0,0 +1,377 @@
+﻿using System.Collections.Generic;
+using Flow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MonteCarloFlowTest
+{
+    [TestClass]
+    public class GeneralWorkProcessTest
+    {
+        [TestMethod]
+        public void ShouldRunSingleStationSingleDeterministicMachine()
+        {
+            GeneralWorkProcess process = new GeneralWorkProcess(new InfiniteBacklog());
+            WorkStation ws1 = new WorkStation(new WipTokenPool(4));
+
+            process.Add(ws1);
+
+            IProbabilityDistribution distribution1 = new DeterministicDistribution(1);
+
+            ws1.AddMachine(new Machine(distribution1));
+
+            FlowTestHelper.TickAndAssert(process, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 1);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 2);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 3);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 4);
+        }
+
+
+        [TestMethod]
+        public void ShouldForKanbanRunTwoStationSingleDeterministicMachineWorkProcess()
+        {
+            GeneralWorkProcess process = new GeneralWorkProcess(new InfiniteBacklog());
+            WorkStation ws1 = new WorkStation(new WipTokenPool(4));
+            WorkStation ws2 = new WorkStation(new WipTokenPool(2)); // Note: Bug in SetWorkstationWipLimit below?
+
+            process.Add(ws1);
+            process.Add(ws2);
+
+            IProbabilityDistribution distribution1 = new DeterministicDistribution(1);
+            IProbabilityDistribution distribution2 = new DeterministicDistribution(2);
+
+            ws1.AddMachine(new Machine(distribution1));
+            ws2.AddMachine(new Machine(distribution2));
+
+            FlowTestHelper.TickAndAssert(process, 1, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 1, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 1, 1, 0, 1);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 1);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 2);
+
+        }
+
+        [TestMethod]
+        public void ShouldForKanbanRunTwoStationDeterministicMachineWorkProcessAndLastWorkStationWithTwoMachines()
+        {
+            GeneralWorkProcess process = new GeneralWorkProcess(new InfiniteBacklog());
+            WorkStation ws1 = new WorkStation(new WipTokenPool(8));
+            WorkStation ws2 = new WorkStation(new WipTokenPool(8));
+
+            process.Add(ws1);
+            process.Add(ws2);
+
+            IProbabilityDistribution distribution1 = new DeterministicDistribution(1);
+            IProbabilityDistribution distribution2 = new DeterministicDistribution(2);
+
+            ws1.AddMachine(new Machine(distribution1));
+            ws2.AddMachine(new Machine(distribution2));
+            ws2.AddMachine(new Machine(distribution2));
+
+            FlowTestHelper.TickAndAssert(process, 1, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 2, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 2, 0, 1);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 2, 0, 2);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 2, 0, 3);
+        }
+
+        [TestMethod]
+        public void ShouldForKanbanPreventExcessWipWhenFirstStationIsFaster()
+        {
+            GeneralWorkProcess process = new GeneralWorkProcess(new InfiniteBacklog());
+            WorkStation ws1 = new WorkStation(new WipTokenPool(3));
+            WorkStation ws2 = new WorkStation(new WipTokenPool(2));
+
+            process.Add(ws1);
+            process.Add(ws2);
+
+            IProbabilityDistribution distribution1 = new DeterministicDistribution(1);
+            IProbabilityDistribution distribution2 = new DeterministicDistribution(2);
+
+            ws1.AddMachine(new Machine(distribution1));
+            ws2.AddMachine(new Machine(distribution2));
+
+            FlowTestHelper.TickAndAssert(process, 1, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 1, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 1, 1, 0, 1);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 1);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 2);
+            FlowTestHelper.TickAndAssert(process, 0, 3, 1, 0, 2);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 3);
+            FlowTestHelper.TickAndAssert(process, 0, 3, 1, 0, 3);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 4);
+            FlowTestHelper.TickAndAssert(process, 0, 3, 1, 0, 4);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 5);
+
+            List<WorkItem> completed = new List<WorkItem>(process.CompletedJobs);
+            Assert.AreEqual(3, completed[0].CycleTime);
+            Assert.AreEqual(4, completed[1].CycleTime);
+            Assert.AreEqual(5, completed[2].CycleTime);
+            Assert.AreEqual(6, completed[3].CycleTime);
+            Assert.AreEqual(6, completed[3].CycleTime);
+
+        }
+
+        [TestMethod]
+        public void ShouldForKanbanRunThreeWorkstationProcess()
+        {
+            GeneralWorkProcess process = new GeneralWorkProcess(new RandomSizeInfiniteBacklog(new DeterministicDistribution(2)));
+            WorkStation ws1 = new WorkStation(new WipTokenPool(1));
+            WorkStation ws2 = new WorkStation(new WipTokenPool(3));
+            WorkStation ws3 = new WorkStation(new WipTokenPool(2)); // Note: Bug in SetWorkstationWipLimit below?
+
+            process.Add(ws1);
+            process.Add(ws2);
+            process.Add(ws3);
+
+            IProbabilityDistribution distribution1 = new DeterministicDistribution(5);
+            IProbabilityDistribution distribution2 = new DeterministicDistribution(30);
+            IProbabilityDistribution distribution3 = new DeterministicDistribution(3);
+
+
+            process[0].AddMachine(new Machine(distribution1));
+            process[1].AddMachine(new Machine(distribution2));
+            process[1].AddMachine(new Machine(distribution2));
+            process[1].AddMachine(new Machine(distribution2));
+            process[2].AddMachine(new Machine(distribution3));
+
+            FlowTestHelper.TickAndAssert(process, 1, 0, 0, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(10, process, 1, 0, 1, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(10, process, 1, 0, 2, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(10, process, 1, 0, 3, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(10, process, 0, 1, 3, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(10, process, 0, 1, 3, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(10, process, 0, 1, 3, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(10, process, 1, 0, 3, 0, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(6, process, 1, 0, 3, 0, 0, 0, 1);
+            FlowTestHelper.TickAndAssert(4, process, 1, 0, 3, 0, 1, 0, 1);
+            //FlowTestHelper.TickAndAssert(10, process, 0, 1, 3, 0, 0, 0, 0);
+
+            //List<WorkItem> completed = new List<WorkItem>(process.CompletedJobs);
+            //Assert.AreEqual(3, completed[0].CycleTime);
+            //Assert.AreEqual(4, completed[1].CycleTime);
+            //Assert.AreEqual(5, completed[2].CycleTime);
+            //Assert.AreEqual(6, completed[3].CycleTime);
+            //Assert.AreEqual(6, completed[3].CycleTime);
+
+        }
+
+        [TestMethod]
+        public void ShouldForConwipRunTwoStationSingleDeterministicMachineWorkProcess()
+        {
+            GeneralWorkProcess process = new GeneralWorkProcess(new InfiniteBacklog());
+
+            WipTokenPool wipLimit = new WipTokenPool(8);
+
+            WorkStation ws1 = new WorkStation(wipLimit);
+            WorkStation ws2 = new WorkStation(wipLimit);
+
+            process.Add(ws1);
+            process.Add(ws2);
+
+            IProbabilityDistribution distribution1 = new DeterministicDistribution(1);
+            IProbabilityDistribution distribution2 = new DeterministicDistribution(2);
+
+            ws1.AddMachine(new Machine(distribution1));
+            ws2.AddMachine(new Machine(distribution2));
+
+            FlowTestHelper.TickAndAssert(process, 1, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 1, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 1, 1, 0, 1);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 1);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 2);
+
+        }
+
+        [TestMethod]
+        public void ShouldForConwipRunTwoStationDeterministicMachineWorkProcessAndLastWorkStationWithTwoMachines()
+        {
+            GeneralWorkProcess process = new GeneralWorkProcess(new InfiniteBacklog());
+
+            WipTokenPool wipLimit = new WipTokenPool(8);
+
+            WorkStation ws1 = new WorkStation(wipLimit);
+            WorkStation ws2 = new WorkStation(wipLimit);
+
+            process.Add(ws1);
+            process.Add(ws2);
+
+            IProbabilityDistribution distribution1 = new DeterministicDistribution(1);
+            IProbabilityDistribution distribution2 = new DeterministicDistribution(2);
+
+            ws1.AddMachine(new Machine(distribution1));
+            ws2.AddMachine(new Machine(distribution2));
+            ws2.AddMachine(new Machine(distribution2));
+
+            FlowTestHelper.TickAndAssert(process, 1, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 2, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 2, 0, 1);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 2, 0, 2);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 2, 0, 3);
+        }
+
+
+        [TestMethod]
+        public void ShouldForConwipRunPooledMachines()
+        {
+            GeneralWorkProcess process = new GeneralWorkProcess(new InfiniteBacklog());
+
+            WipTokenPool wipLimit = new WipTokenPool(8);
+
+            WorkStation ws1 = new WorkStation(wipLimit);
+            WorkStation ws2 = new WorkStation(wipLimit);
+
+            process.Add(ws1);
+            process.Add(ws2);
+
+            IProbabilityDistribution distribution1 = new DeterministicDistribution(4);
+            IProbabilityDistribution distribution2 = new DeterministicDistribution(4);
+
+            ResourcePool pool = new ResourcePool(3);
+
+            ws1.AddMachine(new PooledMachine(distribution1, pool));
+            ws1.AddMachine(new PooledMachine(distribution1, pool));
+            ws1.AddMachine(new PooledMachine(distribution1, pool));
+
+            ws2.AddMachine(new PooledMachine(distribution2, pool));
+            ws2.AddMachine(new PooledMachine(distribution2, pool));
+            ws2.AddMachine(new PooledMachine(distribution2, pool));
+
+            FlowTestHelper.TickAndAssert(process, 3, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 3, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 3, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 3, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 0, 0, 3, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 0, 0, 3, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 0, 0, 3, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 0, 0, 3, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 3, 0, 0, 0, 3);
+        }
+
+        [TestMethod]
+        public void ShouldForConwipHandleProcessingTimeOfZeroForPooledMachine()
+        {
+            GeneralWorkProcess process = new GeneralWorkProcess(new InfiniteBacklog());
+
+            WipTokenPool wipLimit = new WipTokenPool(5);
+
+            WorkStation ws1 = new WorkStation(wipLimit);
+            WorkStation ws2 = new WorkStation(wipLimit);
+            WorkStation ws3 = new WorkStation(wipLimit);
+
+            process.Add(ws1);
+            process.Add(ws2);
+            process.Add(ws3);
+
+            IProbabilityDistribution distribution1 = new DeterministicDistribution(0);
+            IProbabilityDistribution distribution2 = new DeterministicDistribution(1);
+            IProbabilityDistribution distribution3 = new DeterministicDistribution(1);
+            IProbabilityDistribution distribution4 = new DeterministicDistribution(1);
+
+            ResourcePool pool = new ResourcePool(2);
+
+            ws1.AddMachine(new PooledMachine(distribution1, pool));
+            ws1.AddMachine(new PooledMachine(distribution1, pool));
+            ws1.AddMachine(new PooledMachine(distribution1, pool));
+            ws1.AddMachine(new PooledMachine(distribution1, pool));
+            ws1.AddMachine(new PooledMachine(distribution1, pool));
+
+            ws2.AddMachine(new PooledMachine(distribution2, pool));
+            ws2.AddMachine(new PooledMachine(distribution2, pool));
+            ws2.AddMachine(new PooledMachine(distribution2, pool));
+            ws2.AddMachine(new PooledMachine(distribution3, pool));
+            ws2.AddMachine(new PooledMachine(distribution3, pool));
+
+            ws3.AddMachine(new PooledMachine(distribution4, pool));
+            ws3.AddMachine(new PooledMachine(distribution4, pool));
+            ws3.AddMachine(new PooledMachine(distribution4, pool));
+            ws3.AddMachine(new PooledMachine(distribution4, pool));
+            ws3.AddMachine(new PooledMachine(distribution4, pool));
+
+            FlowTestHelper.TickAndAssert(process, 2, 0, 0, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 0, 0, 2, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 0, 0, 0, 0, 2, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 2, 0, 0, 0, 0, 0, 2);
+            FlowTestHelper.TickAndAssert(process, 0, 0, 2, 0, 0, 0, 2);
+            FlowTestHelper.TickAndAssert(process, 0, 0, 0, 0, 2, 0, 2);
+            FlowTestHelper.TickAndAssert(process, 2, 0, 0, 0, 0, 0, 4);
+        }
+
+        [TestMethod]
+        public void ShouldForConwipPreventExcessWipWhenFirstStationIsFaster()
+        {
+            GeneralWorkProcess process = new GeneralWorkProcess(new InfiniteBacklog());
+
+            WipTokenPool wipLimit = new WipTokenPool(4);
+            WorkStation ws1 = new WorkStation(wipLimit);
+            WorkStation ws2 = new WorkStation(wipLimit);
+
+            process.Add(ws1);
+            process.Add(ws2);
+
+            IProbabilityDistribution distribution1 = new DeterministicDistribution(1);
+            IProbabilityDistribution distribution2 = new DeterministicDistribution(2);
+
+            ws1.AddMachine(new Machine(distribution1));
+            ws2.AddMachine(new Machine(distribution2));
+
+            FlowTestHelper.TickAndAssert(process, 1, 0, 0, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 1, 1, 0, 0);
+            FlowTestHelper.TickAndAssert(process, 1, 1, 1, 0, 1);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 1);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 2);
+            FlowTestHelper.TickAndAssert(process, 0, 3, 1, 0, 2);
+            FlowTestHelper.TickAndAssert(process, 1, 2, 1, 0, 3);
+            FlowTestHelper.TickAndAssert(process, 0, 3, 1, 0, 3);
+
+            List<WorkItem> completed = new List<WorkItem>(process.CompletedJobs);
+            Assert.AreEqual(3, completed[0].CycleTime);
+            Assert.AreEqual(4, completed[1].CycleTime);
+            Assert.AreEqual(5, completed[2].CycleTime);
+
+        }
+
+        [TestMethod]
+        public void ShouldForConwipStopProcessingWhenAWorkStationIsStopped()
+        {
+            GeneralWorkProcess process = new GeneralWorkProcess(new InfiniteBacklog());
+
+            WipTokenPool wipLimit = new WipTokenPool(4);
+
+            WorkStation ws1 = new WorkStation(wipLimit);
+            WorkStation ws2 = new WorkStation(wipLimit);
+            WorkStation ws3 = new WorkStation(wipLimit);
+
+            process.Add(ws1);
+            process.Add(ws2);
+            process.Add(ws3);
+
+            IProbabilityDistribution distribution1 = new DeterministicDistribution(1);
+            IProbabilityDistribution distribution2 = new DeterministicDistribution(2);
+
+            ws1.AddMachine(new Machine(distribution1));
+            ws2.AddMachine(new Machine(distribution2));
+            ws2.AddMachine(new Machine(distribution2));
+            ws3.AddMachine(new Machine(distribution1));
+
+            process.Tick(7);
+            FlowTestHelper.TickAndAssert(process, 1, 0, 2, 0, 1, 0, 4);
+
+            ws3.Stop();
+            process.Tick(4);
+            FlowTestHelper.TickAndAssert(process, 0, 0, 0, 4, 0, 0, 5);
+
+            ws3.Start();
+            process.Tick(4);
+
+            FlowTestHelper.TickAndAssert(process, 1, 0, 2, 0, 1, 0, 9);
+        }
+
+    }
+}
